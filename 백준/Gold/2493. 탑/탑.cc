d0017fff6b31ead24a589c718dc124589fe82034
byeok99 <tkdqur3203@gmail.com>
2024-09-13 09:51:01 +0900

    [Gold V] Title: 탑, Time: 88 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,25 +1,21 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 using namespace std; 
 
+int n, temp;
+stack<pair<int,int>> stk;
 int main() {
-	int n, h; 
-	cin >> n; 
-	stack<pair<int, int>> s; 
+	cin.tie(0);
+	ios::sync_with_stdio(0);
 
+	cin >> n; 
 	for (int i = 0; i < n; i++) {
-		cin.tie(0);
-		ios_base::sync_with_stdio(false);
-		cin >> h; 
-		while (!s.empty()) {
-			if (h < s.top().second) {
-				cout << s.top().first << " ";
-				break; 
-			}
-			s.pop(); 
-		}
-		if (s.empty()) {
-			cout << 0 << " "; 
-		}
-		s.push({ i + 1, h }); 
+		cin >> temp; 
+
+		while (!stk.empty() && stk.top().first < temp) stk.pop(); 
+
+		if (stk.empty()) cout << "0 ";
+		else cout << stk.top().second+1 << " ";
+
+		stk.push({ temp, i });
 	}
 }
