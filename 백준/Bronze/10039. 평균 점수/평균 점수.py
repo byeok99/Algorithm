f5c69390db4a36cc5a3853eaae81bf1e6f8a9001
byeok99 <tkdqur3203@gmail.com>
2025-03-22 17:46:36 +0900

    [Bronze IV] Title: 평균 점수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10039. 평균 점수/평균 점수.py
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -1,11 +1,8 @@
-_list = []
-for _ in range(5):
-    _list.append(int(input()))
-
 _sum = 0
 
-for x in _list:
-    if x < 40: x = 40
-    _sum += x
+for _ in range(5):
+    n = int(input())
+    if n < 40: n = 40
+    _sum += n
 
 print(int(_sum / 5))
