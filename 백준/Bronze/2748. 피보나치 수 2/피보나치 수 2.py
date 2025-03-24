0074cfb9249faada528e72901ef6c643a29de4e1
byeok99 <tkdqur3203@gmail.com>
2025-03-24 17:27:47 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,12 +1,11 @@
+n = int(input())
+
 def solve(n):
-    d = [0] * n * 2
-    d[1] = 1
+    memo = [0] * (n+1)
+    memo[1] = 1
 
     for i in range(2, n+1):
-        d[i] = d[i-1] + d[i-2]
+        memo[i] = memo[i-1] + memo[i-2]
+    return memo[n]
 
-    return d[n]
-
-if __name__ == '__main__':
-    n = int(input())
-    print(solve(n))
+print(solve(n))
