ca15860a101b203e63025102f5b5a08025cf7a53
byeok99 <tkdqur3203@gmail.com>
2025-04-03 17:48:44 +0900

    [Silver II] Title: 이항 계수 2, Time: 236 ms, Memory: 54424 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -1,13 +1,8 @@
-a, b = map(int, input().split())
-
+n, k= map(int, input().split())
 memo = [[0] * 1001 for _ in range(1001)]
 
-def bin(n, k):
-    for i in range(n+1):
-        for j in range(min(n,k)+1):
-            if i==0 or i ==j :
-                memo[i][j] = 1
-            else :
-                memo[i][j] = (memo[i-1][j-1] + memo[i-1][j]) % 10007
-bin(a,b)
-print(memo[a][b])
+for i in range(n+1):
+    for j in range(min(i,k)+1):
+        if i==0 or i ==j : memo[i][j] = 1
+        else : memo[i][j] = (memo[i-1][j-1] + memo[i-1][j]) % 10007
+print(memo[n][k])
