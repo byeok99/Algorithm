15dce75b861a7c8db5c3f61e13bd3eb371765133
byeok99 <tkdqur3203@gmail.com>
2025-09-01 15:05:35 +0900

    [Silver V] Title: 뒤집기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,23 +1,4 @@
 s = input()
 
-flag = 0
-target = 0
-cnt = 0
-for i in s:
-    if int(i) == target and flag == 0:
-        flag = 1
-        cnt += 1
-    elif int(i) != target:
-        flag = 0
-
-flag = 0
-target = 1
-_cnt = 0
-for i in s:
-    if int(i) == target and flag == 0:
-        flag = 1
-        _cnt += 1
-    elif int(i) != target:
-        flag = 0
-
-print(min(cnt, _cnt))
+ret = sum((1 if s[i-1] != s[i] else 0) for i in range(1, len(s)))
+print((ret+1)//2)
