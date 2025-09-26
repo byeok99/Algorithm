e0f46832b65aa9a940c885508588ee5a198fcf86
byeok99 <tkdqur3203@gmail.com>
2025-09-26 13:55:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,11 +1,16 @@
-def go(n):
-    if n == 1: return 1
-    if n == 2: return 2
-    if n == 3: return 4
+import sys
+input = sys.stdin.readline
 
-    return go(n-1) + go(n-2) + go(n-3)
+t = int(input())
 
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-        print(go(int(input())))
+dp = [0] * 12
+dp[0] = 1
+dp[1] = 2
+dp[2] = 4
+
+for i in range(3, 12):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n-1])
