7794506b50e385eb1a8296bc499f7b4f2bd8cb18
byeok99 <tkdqur3203@gmail.com>
2025-11-27 14:20:11 +0900

    [Silver III] Title: 수열, Time: 76 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -4,13 +4,12 @@ input = sys.stdin.readline
 n, k = map(int, input().split())
 lst = list(map(int,input().split()))
 
-prefix_sum = [0] * (n+1)
+window_sum = sum(lst[:k])
+max_sum = window_sum
 
-for i in range(1,n+1):
-    prefix_sum[i] = prefix_sum[i-1] + lst[i-1]
+for i in range(k, n):
+    window_sum += lst[i]
+    window_sum -= lst[i-k]
+    max_sum = max(max_sum, window_sum)
 
-ans = float('-inf')
-for i in range(k, n+1):
-    ans = max(ans, prefix_sum[i] - prefix_sum[i-k])
-
-print(ans)
+print(max_sum)
