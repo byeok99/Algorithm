bcae2f8340b8e45c799c4949f6bbee922c6aeba8
byeok99 <tkdqur3203@gmail.com>
2025-12-28 11:39:09 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,32 +1,42 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
 
 int n, c;
-map<int, int> mp, mp_f;
-typedef pair<int, int> pi;
-bool cmp(pi& a, pi& b)
+map<int, int> _map, _map_first;
+vector<pair<int, int>> vec;
+
+bool cmp(pair<int, int> a, pair<int, int> b)
 {
-    if (a.second == b.second) 
-    { 
-		return mp_f[a.first] < mp_f[b.first];
-	}
-    return a.second > b.second;
+    if (a.first == b.first)
+    {
+        return _map_first[a.second] < _map_first[b.second];
+    }
+    return a.first > b.first;
 }
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-    cin >> n >> c; 
-    for (int i=0; i<n; i++) { 
-        cin >> c; 
-        mp[c]++; 
-        if (mp_f[c] == 0) mp_f[c]  = i + 1;
+
+int main()
+{
+    cin >> n >> c;
+
+    for (int i = 0; i < n; i++)
+    {
+        int input;
+        cin >> input;
+        _map[input]++;
+        if (_map_first[input] == 0)
+            _map_first[input] = i + 1;
     }
 
-    vector<pi> v(mp.begin(), mp.end()); 
-    sort(v.begin(), v.end(), cmp); 
-    for (auto i : v) { 
-        for (int j=0; j<i.second; j++) { 
-            cout << i.first << " ";
+    for (auto it : _map)
+        vec.push_back({it.second, it.first});
+
+    sort(vec.begin(), vec.end(), cmp);
+
+    for (auto i : vec)
+    {
+        for (int j = 0; j < i.first; j++)
+        {
+            cout << i.second << " ";
         }
-    } 
+    }
 }
