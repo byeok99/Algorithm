719eaf427621d1bd5a7d1f4ccc082022fb70a89c
byeok99 <tkdqur3203@gmail.com>
2025-12-29 11:02:05 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -0,0 +1,51 @@
+// 10709
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int h, w;
+int ret[101][101];
+char _mp[101][101];
+
+int main()
+{
+    cin >> h >> w;
+
+    for (int i = 0; i < h; i++)
+    {
+        cin >> s;
+        for (int j = 0; j < w; j++)
+        {
+            _mp[i][j] = s[j];
+        }
+    }
+
+    for (int i = 0; i < h; i++)
+    {
+        int cnt = 0;
+        for (int j = 0; j < w; j++)
+        {
+            int it = _mp[i][j];
+
+            ret[i][j] = -1;
+            if (it == 'c')
+            {
+                ret[i][j] = 0;
+                cnt = 1;
+            }
+            else if (cnt > 0)
+            {
+                ret[i][j] = cnt++;
+            }
+        }
+    }
+
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            cout << ret[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
