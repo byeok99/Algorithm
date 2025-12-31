70fc2180be72768739bd289e5cf71a4da9df756d
byeok99 <tkdqur3203@gmail.com>
2025-12-31 12:59:36 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2600 ms, Memory: 3400 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, a, b, _max;
+int n, m, a, b, dp[10001], _max;
 vector<int> v[10001], ret;
 bool visited[10001];
 
@@ -34,9 +34,8 @@ int main()
     {
         fill(&visited[0], &visited[0] + 10001, false);
         ret.push_back(dfs(i));
+        _max = max(ret[i - 1], _max);
     }
-
-    _max = *max_element(ret.begin(), ret.end());
 
     for (int i = 0; i < n; i++)
     {
