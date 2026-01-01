c1e28f811ed57f2375fc551088b0b2e85a0723fa
byeok99 <tkdqur3203@gmail.com>
2026-01-01 10:45:27 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,46 +1,67 @@
+// 50 * 50 * 13
 #include <bits/stdc++.h>
-using namespace std; 
+using namespace std;
 
-int n, m, ret = 987654321, temp;
-vector<pair<int, int>> ch, home; 
-vector<vector<int>> combiList ; 
+int n, m, _map[54][54], ret = INT_MAX;
+vector<pair<int, int>> ck_list, h_list;
 
-void combi(int depth, vector<int> v) {
-	if (v.size() == m) {
-		combiList.emplace_back(v);
-		return; 
-	}
+// void print(vector<int> b)
+// {
+//     for (int i : b)
+//         cout << i << " ";
+//     cout << "\n";
+// }
 
-	for (int i = depth + 1; i < ch.size(); i++) {
-		v.push_back(i); 
-		combi(i, v); 
-		v.pop_back();
-	}
+int calcualte(vector<int> b)
+{
+    int cnt = 0;
+    for (auto home : h_list)
+    {
+        int _min = INT_MAX;
+        for (int i : b)
+        {
+            auto store = ck_list[i];
+            _min = min(abs(home.first - store.first) + abs(home.second - store.second), _min);
+        }
+        cnt += _min;
+    }
+
+    return cnt;
+}
+void combi(int start, vector<int> &b)
+{
+    if (b.size() == m)
+    {
+        ret = min(calcualte(b), ret);
+        return;
+    }
+
+    for (int i = start + 1; i < ck_list.size(); i++)
+    {
+        b.push_back(i);
+        combi(i, b);
+        b.pop_back();
+    }
+    return;
 }
 
-int main() {
-	cin >> n >> m; 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> temp; 
-			if (temp == 1) home.push_back({ i,j });
-			else if (temp == 2) ch.push_back({ i,j }); 
-		}
-	}
+int main()
+{
+    cin >> n >> m;
 
-	combi(-1, vector<int>()); 
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cin >> _map[i][j];
+            if (_map[i][j] == 2)
+                ck_list.push_back({i, j});
+            else if (_map[i][j] == 1)
+                h_list.push_back({i, j});
+        }
+    }
 
-	for (vector<int> cbList : combiList) {
-		int sum = 0;
-		for (pair<int, int> h : home) {
-			int _min = 987654321; 
-			for (int i : cbList) {
-				int _dist = abs(h.first - ch[i].first) + abs(h.second - ch[i].second);
-				_min = min(_min, _dist); 
-			}
-			sum += _min; 
-		}
-		ret = min(ret, sum); 
-	}
-	cout << ret; 
+    vector<int> b;
+    combi(-1, b);
+    cout << ret;
 }
