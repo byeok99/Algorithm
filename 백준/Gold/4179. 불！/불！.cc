d05cbc96daeb030587fa5e6cf33626108273189b
byeok99 <tkdqur3203@gmail.com>
2026-01-05 19:02:04 +0900

    [Gold III] Title: 불!, Time: 64 ms, Memory: 10884 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,66 +1,88 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 987654321;
-int n, m, sy,sx, fire[1000][1000], person[1000][1000], ret;
-char _map[1000][1000];
-const int dy[] = { 0, 1, 0, -1 }; 
-const int dx[] = { 1, 0, -1, 0 };
-queue<pair<int, int>> q; 
+int r, c, sY, sX, ret, visited[1004][1004], person[1004][1004];
+queue<pair<int, int>> q;
+char _map[1004][1004];
+string s;
 
-int main() {
-	fill(&fire[0][0], &fire[0][0] + 1000 * 1000, INF);
-	cin >> n >> m; 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> _map[i][j];
-			if (_map[i][j] == 'J') {
-				sy = i; 
-				sx = j; 
-			}
-			else if (_map[i][j] == 'F') {
-				fire[i][j] = 1; 
-				q.push({ i,j }); 
-			}
-		}
-	}
+int dy[] = {0, -1, 0, 1};
+int dx[] = {1, 0, -1, 0};
 
-	while (q.size()) {
-		int y, x; 
-		tie(y, x) = q.front();
-		q.pop(); 
-		for (int i = 0; i < 4; i++) {
-			int ny = y + dy[i]; 
-			int nx = x + dx[i]; 
-			if (_map[ny][nx] == '#' || fire[ny][nx] != INF) continue; 
-			if (ny >= n || nx >= m || ny < 0 || nx < 0) continue; 
-			fire[ny][nx] = fire[y][x] + 1; 
-			q.push({ ny,nx });
-		}
-	}
+int main()
+{
+    cin >> r >> c;
 
-	person[sy][sx] = 1; 
-	q.push({ sy,sx });
-	while (q.size()) {
-		int y, x; 
-		tie(y, x) = q.front(); 
-		q.pop(); 
-		if (y == n - 1 || x == m - 1 || x== 0 || y ==0) {
-			ret = person[y][x]; 
-			break;
-		}
-		for (int i = 0; i < 4; i++) {
-			int ny = y + dy[i]; 
-			int nx = x + dx[i]; 
-			if (_map[ny][nx] == '#' || person[ny][nx]) continue;
-			if (ny >= n || nx >= m || ny < 0 || nx < 0) continue; 
-			if (fire[ny][nx] != 0 && fire[ny][nx] <= person[y][x] + 1) continue; 
-			person[ny][nx] = person[y][x] + 1; 
-			q.push({ ny,nx }); 
-		}
-	}
+    for (int i = 0; i < r; i++)
+    {
+        cin >> s;
+        for (int j = 0; j < c; j++)
+        {
+            _map[i][j] = s[j];
+            if (s[j] == 'J')
+            {
+                sY = i;
+                sX = j;
+                person[i][j] = 1;
+            }
+            else if (s[j] == 'F')
+            {
+                q.push({i, j});
+                visited[i][j] = 1;
+            }
+        }
+    }
 
-	if (ret != 0) cout << ret << "\n";
-	else cout << "IMPOSSIBLE" << "\n";
+    while (q.size())
+    {
+        int y, x;
+        tie(y, x) = q.front();
+        q.pop();
 
+        for (int i = 0; i < 4; i++)
+        {
+            int ny, nx;
+            ny = y + dy[i];
+            nx = x + dx[i];
+
+            if (ny < 0 || nx < 0 || ny >= r || nx >= c || visited[ny][nx] || _map[ny][nx] == '#')
+                continue;
+
+            visited[ny][nx] = visited[y][x] + 1;
+            q.push({ny, nx});
+        }
+    }
+
+    q.push({sY, sX});
+    while (q.size())
+    {
+        int y, x;
+        tie(y, x) = q.front();
+        q.pop();
+
+        if (y == 0 || x == 0 || y == r - 1 || x == c - 1)
+        {
+            ret = person[y][x];
+            break;
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            int ny, nx;
+            ny = y + dy[i];
+            nx = x + dx[i];
+
+            if (ny < 0 || nx < 0 || ny >= r || nx >= c || _map[ny][nx] == '#' || person[ny][nx])
+                continue;
+            if (visited[ny][nx] <= person[y][x] + 1 && visited[ny][nx] != 0)
+                continue;
+            person[ny][nx] = person[y][x] + 1;
+            q.push({ny, nx});
+        }
+    }
+
+    if (ret != 0)
+        cout << ret << "\n";
+    else
+        cout << "IMPOSSIBLE" << "\n";
 }
