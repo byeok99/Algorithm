ef933d5d4452cb1f48aa59f825ad5e816b18765a
byeok99 <tkdqur3203@gmail.com>
2026-02-25 22:48:06 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int N;
+int memo[1000001];
+
+int main() { 
+    cin >> N;
+    memo[1] = 0;
+
+    for(int i=2; i<=N; i++) {
+        memo[i] = memo[i-1] + 1;
+        if (i % 2 == 0) memo[i] = min(memo[i], memo[i/2] + 1);
+        if (i % 3 == 0) memo[i] = min(memo[i], memo[i/3] + 1);
+    }
+
+    cout << memo[N];
+}
