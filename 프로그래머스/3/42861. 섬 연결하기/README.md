8e28650e68355e6a89913dff92730c6a5e8e323a
byeok99 <tkdqur3203@gmail.com>
2026-09-22 11:35:46 +0900

    [level 3] Title: 섬 연결하기, Time: 1.25 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 77.5 MB, 시간: 0.66 ms
+메모리: 80.2 MB, 시간: 1.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 05일 14:25:53
+2026년 09월 22일 11:35:41
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,37 +1,36 @@
 import java.util.*;
 
 class Solution {
-    static int[] parent;
+    static int[] parent; 
     
     public int solution(int n, int[][] costs) {
         parent = new int[n];
+        
         for(int i=0; i<n; i++) parent[i] = i;
         
         Arrays.sort(costs, (a, b) -> Integer.compare(a[2], b[2]));
         
         int answer = 0;
         int cnt = 0;
-        
-        for(int[] e : costs) {
-            int from = e[0];
-            int to = e[1];
-            int cost = e[2];
+        for(int[] edge : costs) {
+            if (cnt == n-1) break;
+            int to = edge[0];
+            int from = edge[1];
+            int cost = edge[2];
             
-            if (find(from) == find(to)) continue;
+            if (find(to) == find(from)) continue;
             
-            union(from, to);
+            union(to, from);
             
             answer += cost;
-            cnt += 1;
-            
-            if (cnt == n-1) break;   
+            cnt++;
         }
         
         return answer;
     }
     
     static int find(int x) {
-        if (x == parent[x]) return x;
+        if (parent[x] == x) return x;
         return parent[x] = find(parent[x]);
     }
     
@@ -39,6 +38,8 @@ class Solution {
         int rootA = find(a);
         int rootB = find(b);
         
-        if (rootA != rootB) parent[rootB] = rootA;
+        if (rootA == rootB) return;
+        
+        parent[rootB] = rootA;
     }
 }
